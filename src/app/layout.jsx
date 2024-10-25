dbb0b98165eace99454a26a970d74f539305f7e7
nwakakukaks <winnernwakaku123@gmail.com>
2024-10-25 15:50:41 +0100

    fix: connection issues

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,16 +1,17 @@
+'use client'
+
 import { ReactQueryProvider } from "@/components/ReactQueryProvider";
 import { Toaster } from "@/components/ui/toaster";
-import { WrongNetworkAlert } from "@/components/WrongNetworkAlert";
 import { NetworkId } from "@/config";
 import { useState, useEffect } from "react";
 import { NearContext, Wallet } from "@/wallets/near";
 
 import "./globals.css";
 
-export const metadata = {
-  title: "NextJS Boilerplate Template",
-  description: "NextJS Boilerplate Template is a...",
-};
+// export const metadata = {
+//   title: "NextJS Boilerplate Template",
+//   description: "NextJS Boilerplate Template is a...",
+// };
 
 export default function RootLayout({ children }) {
   const wallet = new Wallet({ networkId: NetworkId });
@@ -27,7 +28,7 @@ export default function RootLayout({ children }) {
         <NearContext.Provider value={{ wallet, signedAccountId }}>
           <ReactQueryProvider>
             <div id="root">{children}</div>
-            <WrongNetworkAlert />
+           
             <Toaster />
           </ReactQueryProvider>
         </NearContext.Provider>
